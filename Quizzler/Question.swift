cd35d3afa98fc4e9b1e8d944c6b940de19b1b466
maziseu <maziseu@gmail.com>
2017-11-22 05:39:23 -0800

    This is the starter framework

---
+++ Quizzler/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by mazi on 11/22/17.
+//  Copyright © 2017 mazi. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let answer : Bool
+    let questionText : String
+    
+    init(text : String, correctAnswer : Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+    
+}
---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by mazi on 22/11/2015.
+//  Copyright (c) 2015 mazi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //This action gets called when either the true or false button is pressed.
+    @IBAction func answerPressed(_ sender: AnyObject) {
+        
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
